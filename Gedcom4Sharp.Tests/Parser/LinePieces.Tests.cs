2bf7099b40175b6572848bdee022c7c72fab1f66
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-05-30 18:50:44 +0200

    add LinePieces parser

---
+++ Gedcom4Sharp.Tests/Parser/LinePieces.Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Gedcom4Sharp.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gedcom4Sharp.Tests
+{
+    [TestClass]
+    public class LinePiecesTests
+    {
+        // https://github.com/frizbog/gedcom4j/blob/master/src/test/java/org/gedcom4j/parser/LinePiecesTest.java
+
+        /// <summary>
+        /// Negative test case when the level is non-numeric
+        /// </summary>
+        [TestMethod]
+        public void LinePieces_NegativeBadLevel()
+        {
+            var line = "BAD TAGG additional stuff";
+            var ex = Assert.ThrowsException<Exception>(() => new LinePieces(line, 5));
+            Assert.AreEqual($"Line 5 does not begin with a 1 or 2 digit number for the level followed by a space: {line}", ex.Message);
+        }
+
+        /// <summary>
+        /// Negative test case when the xref is not terminated
+        /// </summary>
+        [TestMethod]
+        public void LinePieces_NegativeBadXref()
+        {
+            var ex = Assert.ThrowsException<Exception>(() => new LinePieces("4 @XREF TAGG additional stuff", 5));
+            Assert.AreEqual("XRef ID begins with @ sign but is not terminated with one on line 5", ex.Message);
+        }
+    }
+}
--- Gedcom4Sharp/Parser/LinePieces.cs
+++ Gedcom4Sharp/Parser/LinePieces.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Gedcom4Sharp.Parser
@@ -9,12 +10,12 @@ namespace Gedcom4Sharp.Parser
         /// <summary>
         /// The current character index into the line
         /// </summary>
-        private int currCharrIndex;
+        private int currCharIndex;
 
         /// <summary>
         /// The characters in the line
         /// </summary>
-        private readonly string chars;
+        private readonly string line;
 
         /// <summary>
         /// The number of the line we are breaking into pieces
@@ -42,47 +43,98 @@ namespace Gedcom4Sharp.Parser
         public string Remainder { get; set; }
 
 
+        public LinePieces(string lineToParse, int lineNum)
+        {
+            this.lineNum = lineNum;
+            line = lineToParse;
+            
+            // iterate over line parts
+            var i = 0;
+            var linePieces = line.Split(' ');
+
+            Level = ProcessLevel(linePieces[i]);
+            i++;
+
+            Id = ProcessXrefId(linePieces[i]);
+            if (!String.IsNullOrEmpty(Id))
+            {
+                i++;
+            }
+
+            Tag = ProcessTag(linePieces[i]);
+            i++;
+
+            Remainder = ProcessRemainder(linePieces, i);
+        }
+
+
         /// <summary>
         ///  Process the level portion of the line
         /// </summary>
-        private void ProcessLevel()
+        private int ProcessLevel(string lvlStr)
         {
-            try
+            var success = int.TryParse(lvlStr, out int lvl);
+            if (success && lvl >= 0 && lvl < 100)
             {
-                currCharrIndex = -1;
-
-                if (Char.IsWhiteSpace(chars[1])){
-                    // Second character in line is a space, so assume a 1-digit level
-                    Level = Convert.ToInt16(Char.GetNumericValue(chars[1]));
-                    currCharrIndex = 2; // Continue parsing at 3rd character in line
-                }
-                else
-                {
-                    // Second character in line is not a space, so assume a 2-digit level
-                    Level = int.Parse(chars.Substring(0, 2));
-                    currCharrIndex = 3; // Continue parsing at 4th character in line
-                }
+                return lvl;
             }
-            catch
+            else
             {
-                throw new Exception($"Line {lineNum} does not begin with a 1 or 2 digit number for the level followed by a space: {chars}");
-            }
-            if(Level < 0 || Level > 99)
-            {
-                throw new Exception($"Line {lineNum} does not begin with a 1 or 2 digit number for the level followed by a space: {chars}");
+                throw new Exception($"Line {lineNum} does not begin with a 1 or 2 digit number for the level followed by a space: {line}");
             }
         }
 
         /// <summary>
         /// Process the remainder of the line
         /// </summary>
-        private void ProcessRemainder()
+        private string ProcessRemainder(string[] chars, int i)
         {
-            if(currCharrIndex < chars.Length)
+            if(chars.Length <= i)
             {
-                Remainder = chars.Substring(currCharrIndex + 1);
+                // no remaining content
+                return String.Empty;
+            }
+            try
+            {
+                return string.Join(" ", chars.Skip(i - 1));
+            }
+            catch
+            {
+                throw new Exception($"All GEDCOM lines are required to have a tag value, but no tag could be found on line {line}");
             }
         }
+
+        /// <summary>
+        /// Process the tag portion of the line
+        /// </summary>
+        private string ProcessTag(string tag)
+        {
+            if (String.IsNullOrEmpty(tag))
+            {
+                throw new Exception($"All GEDCOM lines are required to have a tag value, but no tag could be found on line {line}");
+            }
+            return tag;
+        }
+
         
+        private string ProcessXrefId(string xrefId)
+        {
+            if (xrefId.StartsWith("@"))
+            {
+                if (xrefId.EndsWith("@"))
+                {
+                    // return Tag without surrounding @'s
+                    return xrefId.Substring(1, xrefId.Length - 2);
+                } else
+                {
+                    throw new Exception($"XRef ID begins with @ sign but is not terminated with one on line {lineNum}");
+                }
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+
     }
 }

b8fcfa5820a3d5b40fe84eeeb74a9cf3f0456074
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-08-01 23:32:39 +0200

    auto-init lists & dictionaries

--- Gedcom4Sharp/Models/Gedcom/Gedcom.cs
+++ Gedcom4Sharp/Models/Gedcom/Gedcom.cs
@@ -11,7 +11,7 @@ namespace Gedcom4Sharp.Models.Gedcom
         /// <summary>
         /// All the families in the GEDCOM file. The map is keyed on family cross-reference numbers, and the families themselves are in the value set.
         /// </summary>
-        public Dictionary<string, Family> Families { get; set; }
+        public Dictionary<string, Family> Families { get; set; } = new Dictionary<string, Family>();
 
         /// <summary>
         /// Header information about the GEDCOM
@@ -21,31 +21,31 @@ namespace Gedcom4Sharp.Models.Gedcom
         /// <summary>
         /// All the individuals in the GEDCOM file. The map is keyed on the individual cross-reference numbers and the individuals themselves are in the value set.
         /// </summary>
-        public Dictionary<string, Individual> Individuals { get; set; }
+        public Dictionary<string, Individual> Individuals { get; set; } = new Dictionary<string, Individual>();
 
         /// <summary>
         /// All the multimedia items in the GEDCOM file. The map is keyed by the multimedia cross-reference numbers, and the
         /// multimedia items themselves(well, the metadata about them for 5.5.1) are in the value set.Remember, GEDCOM 5.5.1 multimedia
         /// is not embedded in the GEDCOM, but the GEDCOM contains metadata about the multimedia.
         /// </summary>
-        public Dictionary<string, Multimedia> Multimedia { get; set; }
+        public Dictionary<string, Multimedia> Multimedia { get; set; } = new Dictionary<string, Multimedia>();
 
         /// <summary>
         /// Notes. The map is keyed with cross-reference numbers and the notes themselves are the values.
         /// </summary>
-        public Dictionary<string, NoteRecord> Notes { get; set; }
+        public Dictionary<string, NoteRecord> Notes { get; set; } = new Dictionary<string, NoteRecord>();
 
         /// <summary>
         /// A map of all the source repositories in the GEDCOM file. The map is keyed on the repository cross-reference numbers, and the
         /// repositories themselves are in the value set.
         /// </summary>
-        public Dictionary<string, Repository> Repositories { get; set; }
+        public Dictionary<string, Repository> Repositories { get; set; } = new Dictionary<string, Repository>();
 
         /// <summary>
         /// A map of all the sources in the GEDCOM file. The map is keyed on source cross-reference numbers, and the sources themselves
         /// are in the value set.
         /// </summary>
-        public Dictionary<string, Source> Sources { get; set; }
+        public Dictionary<string, Source> Sources { get; set; } = new Dictionary<string, Source>();
 
         /// <summary>
         /// Information about the GEDCOM submission. There is only one and it is required, so the xref ID has a default.
--- Gedcom4Sharp/Models/Gedcom/NoteRecord.cs
+++ Gedcom4Sharp/Models/Gedcom/NoteRecord.cs
@@ -8,11 +8,11 @@ namespace Gedcom4Sharp.Models.Gedcom
     public class NoteRecord : AbstractElement, HasCitations, HasXref
     {
         public ChangeDate ChangeDate { get; set; }
-        public List<AbstractCitation> Citations { get; set; }
+        public List<AbstractCitation> Citations { get; set; } = new List<AbstractCitation>();
         public string Xref { get; set; }
-        public List<string> Lines { get; set; }
+        public List<string> Lines { get; set; } = new List<string>();
         public StringWithCustomFacts RecIdNumber { get; set; }
-        public List<UserReference> UserReferences { get; set; }
+        public List<UserReference> UserReferences { get; set; } = new List<UserReference>();
 
         public NoteRecord(string xref)
         {

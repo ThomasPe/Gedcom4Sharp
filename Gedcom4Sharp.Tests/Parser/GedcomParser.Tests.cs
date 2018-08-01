aa8b958c7e197fcad306e6e1d41b08fc1fc09a55
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-08-01 23:54:42 +0200

    add test files

--- Gedcom4Sharp.Tests/Parser/GedcomParser.Tests.cs
+++ Gedcom4Sharp.Tests/Parser/GedcomParser.Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Gedcom4Sharp.Parser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,8 +11,20 @@ namespace Gedcom4Sharp.Tests.Parser
 		[TestMethod]
 		public void Test()
         {
-            var parser = new GedcomParser();
-            parser.Load(@"Assets\TortureFiles\EnglishTudorHouse.ged");
+            var gp = new GedcomParser();
+            gp.Load(@"Assets\Samples\EnglishTudorHouse.ged");
+            Debug.WriteLine("done");
+        }
+
+        [TestMethod]
+        public void BadCustomTag()
+        {
+            var gp = new GedcomParser();
+            gp.Load(@"Assets\Samples\Bad_custom_tag.ged");
+            Assert.IsNotNull(gp.Errors);
+            Assert.AreEqual(1, gp.Errors.Count);
+            Assert.IsNotNull(gp.Errors[0]);
+            Assert.IsTrue(gp.Errors[0].Contains("Line 28: Cannot handle tag BUST"));
         }
     }
 }

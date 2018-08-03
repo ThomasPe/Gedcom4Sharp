a389547e40cf746e48660908be47692121ea5b49
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-08-03 17:52:06 +0200

    Finish CustomFactsParser and add test

---
+++ Gedcom4Sharp.Tests/Parser/CustomFactParser.Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Gedcom4Sharp.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gedcom4Sharp.Tests.Parser
+{
+    [TestClass]
+    public class CustomFactParserTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var gp = new GedcomParser();
+            gp.StrictCustomTags = false;
+            gp.Load(@"Assets\Samples\ftmcustomtags.ged");
+            var g = gp.Gedcom;
+            Assert.IsNotNull(g);
+
+            foreach(var e in gp.Errors)
+            {
+                Debug.WriteLine(e);
+            }
+            Assert.AreEqual(0, gp.Errors.Count);
+
+            foreach(var w in gp.Warnings)
+            {
+                Debug.WriteLine(w);
+            }
+            //Assert.AreEqual(0, gp.Warnings.Count);
+
+            var john = g.Individuals["@I1@"];
+            Assert.IsNotNull(john);
+            Assert.AreEqual(19, john.CustomFacts.Count);
+
+            var customFact = john.GetCustomFactsWithTag("_MDCL").FirstOrDefault();
+            Assert.IsNotNull(customFact);
+            Assert.AreEqual("Decapitated, but recovered", customFact.Description.Value);
+            Assert.IsNotNull(customFact.Date);
+            Assert.AreEqual("08 AUG 1978", customFact.Date.Value);
+            Assert.AreEqual("San Antonio, Bexar, Texas, USA", customFact.Place.PlaceName);
+        }
+    }
+}
--- Gedcom4Sharp/Parser/CustomFactParser.cs
+++ Gedcom4Sharp/Parser/CustomFactParser.cs
@@ -23,11 +23,55 @@ namespace Gedcom4Sharp.Parser.Base
                     {
                         _loadInto.Type = ParseStringWithCustomFacts(ch);
                     }
-                    else if (ch.Tag == Tag.CHANGED_DATETIME.Desc()) 
+                    else if (Tag.CHANGED_DATETIME.Desc().Equals(ch.Tag)) 
                     {
                         var changeDate = new ChangeDate();
                         _loadInto.ChangeDate = changeDate;
-                        new ChangeDateParser(_gedcomParser, ch, changeDate);
+                        new ChangeDateParser(_gedcomParser, ch, changeDate).Parse();
+                    }
+                    else if(Tag.DATE.Desc().Equals(ch.Tag))
+                    {
+                        _loadInto.Date = new StringWithCustomFacts(ch.Value);
+                    }
+                    else if(Tag.PLACE.Desc().Equals(ch.Tag))
+                    {
+                        var place = new Place();
+                        _loadInto.Place = place;
+                        new PlaceParser(_gedcomParser, ch, place).Parse();
+                    }
+                    else if(Tag.NOTE.Desc().Equals(ch.Tag))
+                    {
+                        new NoteStructureListParser(_gedcomParser, ch, _loadInto.NoteStructures).Parse();
+                    }
+                    else if(Tag.SOURCE.Desc().Equals(ch.Tag))
+                    {
+                        new CitationListParser(_gedcomParser, ch, _loadInto.Citations).Parse();
+                    }
+                    else if(Tag.CONCATENATION.Desc().Equals(ch.Tag))
+                    {
+                        if(_loadInto.Description == null)
+                        {
+                            _loadInto.Description = ParseStringWithCustomFacts(ch);
+                        }
+                        else
+                        {
+                            _loadInto.Description.Value += ch.Value;
+                        }
+                    }
+                    else if (Tag.CONTINUATION.Desc().Equals(ch.Tag))
+                    {
+                        if(_loadInto.Description == null)
+                        {
+                            _loadInto.Description = new StringWithCustomFacts(ch.Value ?? string.Empty);
+                        }
+                        else
+                        {
+                            _loadInto.Description.Value += "\n" + ch.Value;
+                        }
+                    } 
+                    else
+                    {
+                        UnknownTag(ch, _loadInto);
                     }
                 }
             }
